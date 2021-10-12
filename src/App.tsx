ebef1e35759f5966e1248072e4c1dc31b17decb3
fuaberu <kevinfabel@gmail.com>
2021-10-12 15:31:34 -0300

    basic route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Layout from './pages/layout/Layout';
 import Homepage from './pages/sign-in/Homepage';
+import SignUp from './pages/sign-up/SignUp';
+import MainPage from './pages/mainPage/MainPage';
 
 function App() {
 	return (
 		<div className="App">
-			<Homepage />
+			<Switch>
+				<Route exact path="/" render={() => <Homepage />} />
+				<Route path="/accounts/emailsignup" render={() => <SignUp />} />
+				<Route
+					path="/feed"
+					render={() => (
+						<Layout>
+							<MainPage />
+						</Layout>
+					)}
+				/>
+			</Switch>
 		</div>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.module.css'
+import './index.module.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			<App />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
