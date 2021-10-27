44141edf61d560e512f8ab1815c85b9fa39bf4d2
fuaberu <kevinfabel@gmail.com>
2021-10-27 16:54:13 -0300

    search svg

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -17,6 +17,7 @@ const Header = () => {
 	const [description, setDescription] = useState('');
 	const [place, setPlace] = useState('');
 	const [mediaUrl, setMediaUrl] = useState('');
+	const [search, setSearch] = useState('');
 
 	const userData = useContext(userInfo);
 
@@ -68,8 +69,8 @@ const Header = () => {
 	return (
 		<div className={style.container}>
 			{uploadOpen && (
-				<div className={style.uploadContainer}>
-					<form action="" onSubmit={(e) => handleSubmit(e)}>
+				<div className={style.popupScreen}>
+					<form className={style.popupForm} onSubmit={(e) => handleSubmit(e)}>
 						<input
 							type="file"
 							id="fileInput"
@@ -89,7 +90,10 @@ const Header = () => {
 							value={place}
 							onChange={(e) => setPlace(e.target.value)}
 						/>
-						<SubmitBtn type="submit">Log in</SubmitBtn>
+						<SubmitBtn type="submit">Upload</SubmitBtn>
+						<button type="button" onClick={() => setUploadOpen(!uploadOpen)}>
+							Cancel
+						</button>
 					</form>
 				</div>
 			)}
@@ -100,7 +104,24 @@ const Header = () => {
 						to={`/feed/inbox/${userData.uid}`}
 					/>
 					<li className={style.search}>
-						<input type="text" name="search" id="search" />
+						<label htmlFor="search" className={style.label}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="10"
+								height="10"
+								viewBox="0 0 24 24"
+							>
+								<path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z" />
+							</svg>{' '}
+							Search
+						</label>
+						<input
+							onChange={(e) => setSearch(e.target.value)}
+							value={search}
+							type="text"
+							className={style.input}
+							id="search"
+						/>
 					</li>
 					<li>
 						<ul className={style.links}>
